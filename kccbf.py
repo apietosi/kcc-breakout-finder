4cc0e8dcda5d3430da1122b5ce8823293158b0ca
apietosi <35179623+apietosi@users.noreply.github.com>
2023-03-17 03:55:50 -0400

    Update kccbf.py

--- kccbf.py
+++ kccbf.py
@@ -1,33 +1,15 @@
 import requests
 import json
 
-# Set your API key and secret
-api_key = 'YOUR_API_KEY'
-api_secret = 'YOUR_API_SECRET'
+def get_breakout_price(api_key, api_secret, symbol, timeframe, num_candles):
+    url = f'https://futures.kucoin.com/api/v1/market/candles?symbol={symbol}&timeframe={timeframe}&limit={num_candles}'
+    response = requests.get(url)
 
-# Set the symbol for the futures contract you want to trade
-symbol = 'BTC-USDT'
+    data = json.loads(response.text)
+    candles = data['data']['candles']
 
-# Set the timeframe for the chart data (1min, 5min, 15min, 30min, 60min, 1day, 1week)
-timeframe = '1h'
+    high = max([float(candle[2]) for candle in candles])
+    low = min([float(candle[3]) for candle in candles])
 
-# Set the number of candles to look back for the breakout calculation
-num_candles = 100
-
-# Send a request to the Kucoin Futures API to get the chart data
-url = f'https://futures.kucoin.com/api/v1/market/candles?symbol={symbol}&timeframe={timeframe}&limit={num_candles}'
-response = requests.get(url)
-
-# Parse the response and extract the chart data
-data = json.loads(response.text)
-candles = data['data']['candles']
-
-# Calculate the high and low over the past num_candles candles
-high = max([float(candle[2]) for candle in candles])
-low = min([float(candle[3]) for candle in candles])
-
-# Calculate the breakout price as the average of the high and low
-breakout_price = (high + low) / 2
-
-# Print the breakout price
-print(f'Breakout price: {breakout_price}')
+    breakout_price = (high + low) / 2
+    return breakout_price
